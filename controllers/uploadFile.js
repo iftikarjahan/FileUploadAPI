55d794371d09a1fb90591c3fd631e62eabe08e53
iftikarjahan <iftikarjahan22@gmail.com>
2024-12-17 21:43:16 +0530

    added error handling

--- controllers/uploadFile.js
+++ controllers/uploadFile.js
@@ -1,5 +1,6 @@
 const path=require("path");
 const {StatusCodes}=require("http-status-codes");
+const CustomError=require("../error")
 
 
 const uploadFileController=async (req,res,next)=>{
@@ -7,9 +8,17 @@ const uploadFileController=async (req,res,next)=>{
     // path from where I will upload the extracted file
     
     // check
-    if(!req.files || Object.keys(req.files).length===0){
-        return res.status(StatusCodes.NOT_FOUND).send("No files were uploaded");
-    }
+    if (!req.files) {
+        throw new CustomError.BadRequestError('No File Uploaded');
+      }
+      const productImage = req.files.image;
+      if (!productImage.mimetype.startsWith('image')) {
+        throw new CustomError.BadRequestError('Please Upload Image');
+      }
+      const maxSize = 1024 * 1024;
+      if (productImage.size > maxSize) {
+        throw new CustomError.BadRequestError('Please upload image smaller 1MB');
+      }
     filetbup=req.files.uploadedImage;
     const uploadPath=path.join(__dirname,"..","public","uploads",filetbup.name);
     await filetbup.mv(uploadPath); 
